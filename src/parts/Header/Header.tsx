475fe91a38200d7abb89f47e95ae32d1601e7443
Moisés Reis <118673344+moisreis@users.noreply.github.com>
2024-11-10 19:25:25 -0300

    Update Header.tsx

--- src/parts/Header/Header.tsx
+++ src/parts/Header/Header.tsx
@@ -19,7 +19,7 @@ export async function Header() {
                 {
                     HEADER.links.map((link) => {
                         return (
-                            <Button variant='Link' content={link.page.categoryTitle} />
+                            <Button variant='Link' content={link.page.categoryTitle} key={link.id} />
                         );
                     })
                 }
@@ -31,7 +31,7 @@ export async function Header() {
                 {
                     HEADER.socialMedia.map((link) => {
                         return (
-                            <Button variant='Link' content={`@${link.socialMediaLabel}`} />);
+                            <Button variant='Link' content={`@${link.socialMediaLabel}`} key={link.id} />);
                     })
                 }
                 <Button variant='Primary' content={HEADER.mainButtonText}/>
